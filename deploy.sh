1fa9e45ce337b724aca44aa986c7d6c4e1f69428
asarishahak <jasonstatham@gamil.com>
2021-12-18 19:38:45 +0530

    commited by my self

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,5 @@ this line is updated now only...
 shell scripting looking little harder so i have to practies more
 
 it is updated by the my self for checkig the fetch command
+
+it is updated for the checking the ssh url
